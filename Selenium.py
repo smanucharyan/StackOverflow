49bf399759cd8b993a2a2e4acd739c3dde3a4b58
Yervand Abrahamian <yabrahamian@gmail.com>
2020-05-24 14:11:42 +0400

    Initial commit

---
+++ Selenium.py
@@ -0,0 +1,17 @@
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.keys import Keys
+
+driver = Chrome()
+with Chrome() as driver:
+    driver.get("http://the-internet.herokuapp.com/login")
+    driver.maximize_window()
+    url = driver.current_url
+    title = driver.title
+    print(title)
+    print(url)
+
+    login_field = driver.find_element_by_id("username")
+    login_field.send_keys("tomsmith")
+    password_field = driver.find_element_by_id("password")
+    password_field.send_keys("SuperSecretPassword!" + Keys.ENTER)
+    driver.quit()
