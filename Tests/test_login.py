fa2d90f8f95192810c97bf26165818fcf5fdf675
smanucharyan <66773121+smanucharyan@users.noreply.github.com>
2020-06-11 14:45:19 +0400

    Add files via upload

---
+++ Tests/test_login.py
@@ -0,0 +1,32 @@
+import pytest
+import os
+from selenium import webdriver
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from POM import login_page
+
+class TestLogin():
+
+    @pytest.fixture
+    def login(self, request):
+        _chromedriver = os.path.join(os.getcwd(), 'vendor', 'chromedriver')
+        if os.path.isfile(_chromedriver):
+            _service = Chrome(executable_path=chromedriver)
+            driver_ = webdriver.Chrome(service=_service)
+        else:
+            driver_ = webdriver.Chrome()
+
+        def quit():
+            driver_.quit()
+
+        request.addfinalizer(quit)
+        return login_page.LoginPage(driver_)
+
+    def test_valid_credentials(self, login):
+        login.with_("tomsmith", "SuperSecretPassword!")
+        assert(login.success_message_present())
+
+
+
+
